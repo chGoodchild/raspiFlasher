280c9ea3695da9a5760011ff24f04159d88091be
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-05-12 21:29:31 +0200

    Switching to standalone

--- yaml_launcher.py
+++ yaml_launcher.py
@@ -3,6 +3,7 @@ import subprocess
 import requests
 import hashlib
 import os
+import tarfile
 
 def file_checksum_matches(image_path, expected_checksum):
     sha256 = hashlib.sha256()
@@ -27,67 +28,57 @@ def download_image(image_url, image_path, expected_checksum):
         else:
             raise Exception(f"Failed to download the file: Status code {response.status_code}")
 
-
 def install_balena_etcher():
-    # Check if Balena Etcher is installed
     if not is_etcher_installed():
-        print("Balena Etcher is not installed, proceeding with installation...")
+        print("Balena CLI is not installed, proceeding with installation...")
         install_etcher()
-        # Add the installation path to the system PATH
-        os.environ["PATH"] += os.pathsep + '/opt/balenaEtcher'
         if not is_etcher_installed():
-            raise Exception("Failed to install Balena Etcher.")
+            raise Exception("Failed to install Balena CLI.")
     else:
-        print("Balena Etcher is already installed.")
+        print("Balena CLI is already installed.")
 
 def is_etcher_installed():
-    """Check if Balena Etcher is in PATH and can be executed."""
     try:
-        subprocess.run(['balena-etcher-electron', '--version'], check=True, stdout=subprocess.PIPE)
+        subprocess.run(['balena', '--version'], check=True, stdout=subprocess.PIPE)
         return True
     except (FileNotFoundError, subprocess.CalledProcessError):
         return False
 
 def install_etcher():
-    # Download and execute the script to add the Balena Etcher repository
-    subprocess.run(['curl', '-fsSL', 'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh', '-o', 'etcher_setup.sh'], check=True)
-    subprocess.run(['sudo', 'bash', 'etcher_setup.sh'], check=True)
-    # Update the package list
-    subprocess.run(['sudo', 'apt-get', 'update'], check=True)
-    # Install Etcher
-    subprocess.run(['sudo', 'apt-get', 'install', 'balena-etcher-electron', '-y'], check=True)
-    print("Installation of Balena Etcher completed.")
- 
+    balena_cli_zip = 'balena-cli-v18.2.2-linux-x64-standalone.zip'
+    download_url = 'https://github.com/balena-io/balena-cli/releases/download/v18.2.2/' + balena_cli_zip
+    try:
+        print("Downloading Balena CLI...")
+        response = requests.get(download_url)
+        with open(balena_cli_zip, 'wb') as f:
+            f.write(response.content)
+
+        print("Unpacking Balena CLI...")
+        with tarfile.open(balena_cli_zip, "r:gz") as tar:
+            tar.extractall()
+
+        print("Balena CLI installed successfully.")
+    except Exception as e:
+        print(f"Failed to install Balena CLI: {str(e)}")
+        raise
+
 def run_flash_script_from_yaml(yaml_file_path):
-    # Load the YAML file to get configuration settings
     with open(yaml_file_path, 'r') as file:
         config = yaml.safe_load(file)
-
-    # Extract arguments from the YAML file
     sd_card = config['sd_card']
     image_url = config['image_url']
     image_path = config['image_path']
     ssid = config['ssid']
     wifi_password = config['wifi_password']
     expected_checksum = config['expected_checksum']
-
-    # Ensure Balena Etcher is installed
     install_balena_etcher()
-
-    # Check and download the image if necessary
     download_image(image_url, image_path, expected_checksum)
+    command = ['python3', 'raspiFlasher.py', sd_card, image_path, ssid, wifi_password, expected_checksum]
 
-    # Construct the command to run the flash script
-    command = [
-        'python3', 'raspiFlasher.py', 
-        sd_card, image_path, ssid, wifi_password, expected_checksum
-    ]
-    
-    # Launch the script
     print("Launching the SD card flashing script...")
     subprocess.run(command, check=True)
 
 if __name__ == '__main__':
-    # Specify the path to your YAML configuration file
     yaml_file_path = 'config.yaml'
     run_flash_script_from_yaml(yaml_file_path)
+
