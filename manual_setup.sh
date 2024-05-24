cb0a8ed6a4ef5968c9762b66cefe3baa6a8b1499
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-05-24 15:14:27 +0200

    Added things to manual setup

--- manual_setup.sh
+++ manual_setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Initial setup and system configuration
 
 sudo apt update -y
 sudo apt upgrade -y
+
+# Install necessary packages
+sudo apt-get install -y qemu-user-static binfmt-support meld
+
 
 # Install Docker
 echo "Installing Docker..."
@@ -12,3 +17,9 @@ sudo usermod -aG docker $(whoami)
 # Install Tailscale
 echo "Installing Tailscale..."
 curl -fsSL https://tailscale.com/install.sh | sudo sh
+
+# Download and run the manual setup script
+wget https://raw.githubusercontent.com/chGoodchild/raspiFlasher/master/manual_setup.sh
+chmod +x manual_setup.sh
+./manual_setup.sh
+
