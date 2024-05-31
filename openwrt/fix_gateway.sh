d4c5712a73b85c1c1766c0d8acd8abc813414be0
Chandran Goodchild <ch.goodchild@embedsmart.de>
2024-05-31 15:28:22 +0200

    Installed kmod-nf-nat and kmod-ipt-tproxy

--- openwrt/fix_gateway.sh
+++ openwrt/fix_gateway.sh
@@ -14,11 +14,21 @@ ip addr show eth0.3
 service network restart
 service firewall restart
 service opennds restart
+service dnsmasq restart
 
 nft --version
 # nftables v1.0.8 (Old Doc Yak #2)
 
 opkg install nftables
 # Package nftables-json (1.0.8-1) installed in root is up to date.
+
+
+nft add table ip nds_nat
+nft add chain ip nds_nat ndsOUT { type nat hook postrouting priority 100 \; }
+nft add rule ip nds_nat ndsOUT ip daddr 192.168.3.1 tcp dport 2050 counter accept
+nft add rule ip nds_nat ndsOUT tcp dport 80 counter dnat to 192.168.3.1:2050
+
+opkg update
+opkg install kmod-nf-nat kmod-ipt-tproxy
 
 
